c086f3c3658bdc4b4cbf4b7744d9a9f1736240c5
FreedomWriter <natalienetworks@gmail.com>
2019-10-08 22:07:04 -0500

    imported BrowserRouter as Router

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [teamMember, setTeamMember] = useState({
+    name: "",
+    email: "",
+    role: ""
+  });
   return <div></div>;
 }
 
